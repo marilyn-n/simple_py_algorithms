188843cb652dc324cd581d3b48718f25756f707c
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-07-08 17:50:18 -0500

    starter code

--- main.py
+++ main.py
@@ -9,3 +9,53 @@
 # 		Like a diamond in the sky. 
 # Twinkle, twinkle, little star, 
 # 	How I wonder what you are
+
+
+
+# 2. Write a Python program to display the current date and time.
+# Sample Output : 
+# Current date and time : 
+# 2014-07-05 14:34:14
+
+
+# 3. Write a Python program which accepts the radius of a circle from the user and compute the area.
+# Sample Output : 
+# r = 1.1
+# Area = 3.8013271108436504
+
+# 4. Write a Python program which accepts the users first and last name and print them in reverse 
+# order with a space between them.
+
+# 5. Write a Python program which accepts a sequence of comma-separated numbers from 
+# user and generate a list and a tuple with those numbers. Go to the editor
+# Sample data : 3, 5, 7, 23
+# Output : 
+# List : ['3', ' 5', ' 7', ' 23'] 
+# Tuple : ('3', ' 5', ' 7', ' 23')
+
+# 6. Write a Python program to count the number of characters (character frequency) in a string.
+# Sample String : google.com'
+# Expected Result : {'o': 3, 'g': 2, '.': 1, 'e': 1, 'l': 1, 'm': 1, 'c': 1}
+
+# 7. Write a Python program to get the largest number from a list
+
+# 8. Write a Python program to convert temperatures to and from celsius, fahrenheit.
+# [ Formula : c/5 = f-32/9 [ where c = temperature in celsius and f = temperature in fahrenheit ] 
+# Expected Output : 
+# 60°C is 140 in Fahrenheit
+# 45°F is 7 in Celsius 
+
+# 9. Write a Python function to sum all the numbers in a list. Go to the editor
+# Sample List : (8, 2, 3, 0, 7)
+# Expected Output : 20 
+
+# 10. Write a Python program which iterates the integers from 1 to 50. For multiples of three print "Fizz" 
+# instead of the number and for the multiples of five print "Buzz". 
+# For numbers which are multiples of both three and five print "FizzBuzz".
+# Sample Output : 
+# fizzbuzz
+# 1
+# 2
+# fizz
+# 4 
+# buzz
