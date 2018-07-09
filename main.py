7f43b46d76decb2444388d34ab6e0bf971cddfeb
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-07-09 12:13:31 -0500

    Display current date and time

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-# 1. Write a Python program to print the following string in a specific format (see the output). 
-# Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world 
-# so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" 
-# Output :
-
-# Twinkle, twinkle, little star,
-# 	How I wonder what you are! 
-# 		Up above the world so high,   		
-# 		Like a diamond in the sky. 
-# Twinkle, twinkle, little star, 
-# 	How I wonder what you are
-
-
-
-# 2. Write a Python program to display the current date and time.
-# Sample Output : 
-# Current date and time : 
-# 2014-07-05 14:34:14
-
-
-# 3. Write a Python program which accepts the radius of a circle from the user and compute the area.
-# Sample Output : 
-# r = 1.1
-# Area = 3.8013271108436504
-
-# 4. Write a Python program which accepts the users first and last name and print them in reverse 
+# 1. Write a Python program which accepts the users first and last name and print them in reverse 
 # order with a space between them.
 
 name = input('Whats your name?')
@@ -35,6 +10,39 @@ def sayHello (name, lastName):
 
 print(sayHello(name, lastName))
 
+# 2. Write a Python program to display the current date and time.
+# Sample Output : 
+# Current date and time : 
+# 2014-07-05 14:34:14
+
+import datetime
+
+time = datetime.datetime.now()
+
+def currentDateAndTime (time):
+  dateAndTime = str(time)
+  return 'current date and time is: ' + dateAndTime
+  
+print(currentDateAndTime(time))
+
+
+
+# 3. Write a Python program which accepts the radius of a circle from the user and compute the area.
+# Sample Output : 
+# r = 1.1
+# Area = 3.8013271108436504
+
+# 4. Write a Python program to print the following string in a specific format (see the output). 
+# Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world 
+# so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" 
+# Output :
+
+# Twinkle, twinkle, little star,
+# 	How I wonder what you are! 
+# 		Up above the world so high,   		
+# 		Like a diamond in the sky. 
+# Twinkle, twinkle, little star, 
+# 	How I wonder what you are
 
 # 5. Write a Python program which accepts a sequence of comma-separated numbers from 
 # user and generate a list and a tuple with those numbers. Go to the editor
@@ -68,4 +76,4 @@ print(sayHello(name, lastName))
 # 2
 # fizz
 # 4 
-# buzz
+# buz
